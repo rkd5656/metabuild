c6a527599ef238f47590d7e39ab15d742c02314a
rkd5656 <rkdqudcjs12@naver.com>
2019-01-25 17:31:02 +0900

    d

--- src/main/webapp/resources/app/appController.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-angular.module('meta-app').controller('AppCtrl', function ($scope, $rootScope) {
-    $rootScope.userList = [
-        {id: 'misun-kim', name: '김미선', position: '선임', department: '지능융합기술실'},
-        {id: 'gildong', name: '홍길동', position: '전임', department: 'SW연구센터'}
-    ];
-
-    $rootScope.commonText = '메타빌드 신입사원 교육용 AngularJS1 템플릿입니다.'
-});
